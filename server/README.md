d3d54c0908c8b336fb56f45a618afd6e903706bc
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-12-11 13:58:08 -0800

    Basic Django setup

---
+++ server/README.md
@@ -0,0 +1,7 @@
+Quickstart
+==========
+
+    make install # install deps
+    source .env/bin/activate # to use the virtualenv (every terminal you use)
+    python manage.py migrate # install tables
+    python manage.py runserver # run devserver with auto refresh
---
+++ server/src/airbender/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'airbender.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+)
