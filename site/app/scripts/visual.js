31e8f9a14732801e534fbeb9ca2d70ec0d7ec4f3
Amit Kirdatt <akirdatt@walmartlabs.com>
2014-12-11 23:01:33 -0600

    paint several rectangles based on data

--- site/app/scripts/visual.js
+++ site/app/scripts/visual.js
@@ -6,10 +6,30 @@ angular.module('airbender.directives', [])
       link: function(scope, element, attrs) {
         d3Service.d3().then(function(d3) {
            
-          var svgContainer = d3.select(element[0]).append("svg")
-                                    .attr("width", 200)
-                                    .attr("height", 200)
-                                    .style("border", "1px solid black");
+          var svg = d3.select(element[0]).append("svg")
+                                         .attr("width", 800)
+                                         .attr("height", 600);
+          var data = [1, 2, 3];
+
+          scope.render = function() {
+            svg.selectAll("*").remove();
+            
+            svg.selectAll("rect")
+               .data(data)
+               .enter()
+               .append("rect")
+               .attr("width", 50)
+               .attr("height", 60)
+               .attr("x", 10) 
+               .attr("y", function(d, i){
+                  return i * 65;
+                }) 
+               .style("border", "1px solid black");
+
+          }
+
+          scope.render();
+
 
           window.onresize = function() {
             scope.$apply();
